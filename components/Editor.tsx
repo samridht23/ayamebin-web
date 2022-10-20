3da141d2852177bd6149d0a7a784730cb036f274
samridh <samridht23@gmail.com>
2022-10-20 17:15:50 +0530

    updated

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { FileTextIcon } from "@radix-ui/react-icons";
-import highlight from "highlight.js";
+import hljs from "highlight.js";
 import Link from "next/link";
 const Editor = () => {
-  const [content, setContent] = useState("");
-
-  const [highlightContent, setHighlightContent] = useState(content);
+  const content = `import test from "test"
+    const dotenv = require('dotenv').config()`;
   useEffect(() => {
-    highlight.highlightAll();
-  }, []);
+    hljs.highlightAll();
+  });
   return (
     <>
-      <div className="w-full flex justify-center py-12 px-12 lg:px-[24rem] ">
+      <div className="w-full flex text-sm justify-center py-12 px-12 lg:px-[24rem] ">
         <div className=" w-full h-screen ">
           <div className="w-full bg-[#161B22] border border-[#525A67] flex justify-between text-white border rounded-t-lg  items-center px-4 py-2">
             <div className="text-[#959CA4]">
@@ -22,12 +21,9 @@ const Editor = () => {
             </div>
           </div>
           <div className="border-x border-b h-96 rounded-b-lg border-[#525A67]">
-            <textarea
-              className="w-full h-full rounded-b-lg whitespace-nowrap resize-none bg-[#0D1117] text-gray-400 text-sm outline-none"
-              onChange={(e) => setContent(e.target.value)}
-            >
-              {setHighlightContent}
-            </textarea>
+            <pre>
+              <code>{content}</code>
+            </pre>
           </div>
         </div>
       </div>
