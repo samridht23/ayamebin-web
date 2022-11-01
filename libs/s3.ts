45db06c651569cc9d1d0f9ca416947da7f05e3b2
samridh <samridht23@gmail.com>
2022-11-01 22:18:37 +0530

    encryption added

--- libs/s3.ts
+++ libs/s3.ts
@@ -3,10 +3,11 @@ import {
   PutObjectCommand,
   GetObjectCommand,
 } from "@aws-sdk/client-s3";
-import cuid from "cuid";
+import { v4 as uuidv4 } from "uuid";
 
 const REGION = process.env.REGION;
 
+// creating new instance of s3
 const s3Client = new S3Client({
   region: REGION,
   credentials: {
@@ -14,10 +15,13 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.SECRET_KEY,
   },
 });
+// function to upload
+// take content as string but make it number since it will receive
+// encrypted hex code
 export const upload = async (content: string) => {
   const params = {
     Bucket: process.env.BUCKET,
-    Key: `pastes/${cuid.slug()}`,
+    Key: `pastes/${uuidv4()}`,
     Body: content,
   };
   try {
@@ -28,5 +32,6 @@ export const upload = async (content: string) => {
     console.log(err);
   }
 };
+export const getContent = async();
 
 module.exports = { upload };
--- pages/api/paste/index.ts
+++ pages/api/paste/index.ts
@@ -1,14 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { upload } from "../../../libs/s3";
+import cuid from "cuid";
+const aes256 = require("aes256");
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     return res.status(405).json({ message: "code to be coded" });
   }
   if (req.method === "POST") {
+    const key = cuid.slug();
     const payload = req.body;
+    const encryptedpayload = aes256.encrypt(key, payload);
     try {
-      const content = upload(payload);
+      // encrypted payload has been attached
+      const content = upload(encryptedpayload);
       console.log(content);
       return res.status(200).json({ message: "code has been uploaded" });
     } catch (err) {
@@ -20,7 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 export const config = {
   api: {
     bodyParser: {
-      sizeLimit: "5mb",
+      sizeLimit: "2mb",
     },
   },
 };
