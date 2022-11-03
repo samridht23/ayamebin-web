91ad8ae221ab7ea2f40f75ec674dad4f6867a4b8
samridh <samridht23@gmail.com>
2022-11-04 00:07:28 +0530

    test

---
+++ env.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ACCESS_KEY: string;
+      SECRET_KEY: string;
+      BUCKET: string;
+      REGION: string;
+      ENDPOINT: string;
+    }
+  }
+}
+export {};
