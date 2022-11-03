4a7912cce3a956560f6cb531653e736268e26955
samridh <samridht23@gmail.com>
2022-11-03 22:54:56 +0530

    debugging

--- libs/s3.ts
+++ libs/s3.ts
@@ -16,24 +16,32 @@ const s3Client = new S3Client({
   },
 });
 export const upload = async (payload: string) => {
-  const params = {
-    Bucket: process.env.BUCKET,
-    Key: `pastes/${uuidv4()}`,
-    Body: payload,
-  };
   try {
-    const data = await s3Client.send(new PutObjectCommand(params));
+    const data = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.BUCKET,
+        Key: `pastes/${uuidv4()}`,
+        Body: payload,
+      })
+    );
     console.log("uploaded");
     return data;
   } catch (err) {
     console.log(err);
   }
 };
-export const getContent = async (key: string) => {
+
+export const getObject = async (key: string) => {
   try {
+    const data = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: process.env.Bucket,
+        Key: key,
+      })
+    );
+    return data;
   } catch (err) {
     console.log(err);
   }
 };
-
 module.exports = { upload };
